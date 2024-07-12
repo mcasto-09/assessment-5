d3a4dced3b1baf8a21fca38d86c720482cf3d765
Mary Castorani <mgcastorani@gmail.com>
2024-07-12 15:43:17 -0600

    quiz.js complete

--- src/quiz.js
+++ src/quiz.js
@@ -18,7 +18,7 @@ await client.connect();
 export const problem01 = await client.query(`
   SELECT * FROM cupcakes
   WHERE name = 'vanilla';
-`); 
+`);
 console.log('-- Problem 01 --');
 console.log(problem01?.rows);
 
@@ -46,22 +46,36 @@ console.log('-- Problem 03 --');
 console.log(problem03?.rows);
 
 // Problem 04
-export const problem04 = await client.query (`
+export const problem04 = await client.query(`
   SELECT c.name, (SUM(o.num_cupcakes),0) AS sum
   FROM cupcakes c
   LEFT JOIN orders o ON c.id = o.cupcake_id
   GROUP BY c.name
   ORDER BY c.name;
 `)
+console.log('---Problem 04---')
+console.log(problem04?.rows)
 
 // Problem 05
-export const problem05 = null;
-// console.log('-- Problem 05 --');
-// console.log(problem05?.rows);
+export const problem05 = await client.query(`
+SELECT c.email, SUM(o.num_cupcakes) AS total
+FROM customers c
+JOIN orders o ON c.id = o.customer_id
+GROUP BY c.email
+ORDER BY total DESC;
+`)
+console.log('-- Problem 05 --');
+console.log(problem05?.rows);
 
 // Problem 06
-export const problem06 = null;
+export const problem06 = await client.query(`
+SELECT DISTINCT c.fname, c.lname, c.email
+FROM customers c
+JOIN orders o ON c.id = o.customer_id
+JOIN cupcakes cp ON o.cupcake_id = cp.id
+WHERE o.processed = 't' AND cp.name = 'funfetti';
+`)
 console.log('-- Problem 06 --');
-// console.log(problem06?.rows);
+console.log(problem06?.rows);
 
 client.end();
